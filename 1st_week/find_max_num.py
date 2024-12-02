18a112989a60a48fcd805e5f7f24e7f8e2907866
hongsDev <responsibility91@naver.com>
2024-12-03 00:46:39 +0900

    hongs commit

--- 1st_week/find_max_num.py
+++ 1st_week/find_max_num.py
@@ -7,6 +7,9 @@ def find_max_num2(arr):
         if is_max:
             return num
 
+
+        #hongs
+
 print(find_max_num2([1, 0, 32, 56, 6, 7, 123123, 78, 2, 2, 324, 2, 34, 12, 31, 23, 12, 4124]))
 
 
